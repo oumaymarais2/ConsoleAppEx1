2cac6987ee7433a81104c25c3120fa8f208e592f
oumaymarais2 <oumayma.rais@usmba.ac.ma>
2022-10-28 11:50:44 +0200

    Exercice1

--- ConsoleAppEx1/Program.cs
+++ ConsoleAppEx1/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleAppEx1
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int n;
+            Console.WriteLine("Entrer un nbre:");
+            n = int.Parse(Console.ReadLine());
+            if (n == 0 || n == 1)
+            {
+                Console.WriteLine(n + "n'est pas un nbre premier");
+                Console.ReadLine();
+            }
+            else
+            {
+                for (int i = 2; i <= n / 2; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        Console.WriteLine(n + "n'est pas un nbre premier");
+                        Console.ReadLine();
+                    }
+                }
+            }
+            Console.WriteLine(n + "est  un nbre premier");
+            Console.ReadLine();
         }
     }
 }
